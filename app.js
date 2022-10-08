950ac55d77a9f13ed57e759cb29132cb23d17a51
sidhawkss <duser@d.com>
2022-10-08 12:43:13 -0300

    Input validation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Joi = require('joi')
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -10,58 +10,84 @@ const courses = [
     {id:3, name: 'course3'}
 ]
 
+// Function to validate course.
+function validateCourse(course){
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const result = schema.validate(course);
+
+    return result;
+}
+
+// Principal route
 app.get('/api/courses', (req,res) => {
+    
     res.send(courses);
+
 })
 
+// Get course by id
 app.get('/api/courses/:id', (req,res) =>{
+
     const course = courses.find(c => c.id === parseInt(req.params.id));
+    
     if(!course){
         res.status(404).send('The id was not found');
     } else {
-        res.send(course)
+        res.send(course);
     }
+
 })
 
-
+// Add an item to courses
 app.post('/api/courses', (req,res) => {
-    const schema = {
+
+    // Validation of the user input
+    const schema = Joi.object({
         name: Joi.string().min(3).required()
-    }
+    })
 
-    const result = new Joi.ValidationError(req.body, schema);
-    //sconsole.log(result);
-    console.log(result.details);
-
-    if(!req.body.name || req.body.name.length <  3){
-        res.status(404).send('Name is required minimum 3 characteers.');
-        return;
-    }
+    const { error } = schema.validate(req.body);
 
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
-    courses.push(course);
-    res.send(course);
+
+    // Identify the input and send a message to the user.
+    if(error){
+        res.status(404).send("Error, input a valid string");
+    }else{
+        courses.push(course);
+        res.send(course);
+    }
 
 ;})
 
+app.put('/api/courses/:id', (req,res) =>{
+    
+    // Update and course
+    const course  = courses.find(c => c.id === parseInt(req.params.id));
+
+    if(!course){
+        res.status(404).send("The course was not found!")
+    };
+
+    const {error} = validateCourse(req.body);
+    if(error){
+        res.status(404).send("Error, input a valid string");
+        return;
+    };
+
+    course.name = req.body.name;
+    res.send(course);
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, () =>{
-    console.log('listening on port 3000...');
+app.listen(5000, () =>{
+    console.log('listening on port 5000...');
 })
